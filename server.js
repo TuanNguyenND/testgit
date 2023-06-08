0edb0893e53f6f4ffc88b511a79fca40dbaa87bf
TuanNguyenND <tuannguyennd1012@gmail.com>
2023-06-08 19:39:14 +0900

    testgit

---
+++ server.js
@@ -0,0 +1,12 @@
+const htpp = require('http');
+const server = hgttp.createServer((req, res) => {
+    console.log('run request...')
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h3> Hello world </h3>');
+    res.write('<h2> from Tuan Nguyen</h2>');
+    res.end();
+
+})
+server.listen(3000, 'localhost', () => {
+    console.log('Node.JS server is running on port: 3000');
+})
